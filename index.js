0d2b8ac8de74539ef61c803bfc5f2d7d795693ba
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-10-04 07:45:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(drivers){
   return drivers.map(function(item){
-    return item.name + ' is from ' + item.name
+    return item.name + ' is from ' + item.hometown
   })
 }
